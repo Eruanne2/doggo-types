7f27660068e2e7eeacf4ad38a0c6723f9fc9aa63
Charis <bluegrassgal222@gmail.com>
2021-10-03 21:16:10 -0700

    style photo gallery

--- src/components/ImageIndex.tsx
+++ src/components/ImageIndex.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import '../assets/ImageIndex.css';
+import loadingGif from '../assets/loading.gif'
 import { useAppSelector, useAppDispatch } from '../redux/typedHooks';
 import { getBreedImages } from '../redux/dogActions';
 
@@ -12,11 +13,28 @@ export default function ImageIndex(props: { selected: string}) {
     dispatch(getBreedImages(props.selected) as any) // better solution?
   }, [props.selected]);
 
+
+  let imgGrid: Array<Array<string>> = [[], [], [], []];
+  imgs.forEach((img, i) => imgGrid[i % 4].push(img))
+
   return (
-    <div className='img-grid'>
-      {imgs.map((img, i) => {
-        return <img key={i} src={img}/>
-      })}
+    <div className='img-index-container'>
+      {
+        imgs.length === 0 ?
+        <div className='loading-container'>
+          <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+        :
+        <div className='img-grid'>
+          {imgGrid.map((row,i) => 
+            <ul key={i} className='img-row'>
+              {row.map((img,i) =>
+                <img key={i} src={img}/>
+              )}
+            </ul>
+          )}
+        </div>
+      }
     </div>
   )
 }
